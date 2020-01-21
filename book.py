7924e11352d32767284d538c7a335fa6ac9a77b1
Jared Eipp <jeipp1@binghamton.edu>
2020-01-21 15:03:38 -0500

    First push from a different computer

--- book.py
+++ book.py
@@ -8,12 +8,32 @@ class Person:
 	def __init__(self, n):
 		self.name = n
 
-	
 	def addAddress(self, a):
 		self.address = a
-
+	
 	def addEmail(self, e):
 		self.email = e
-
+	
 	def addPhone(self, pn):
 		self.phone = pn
+		
+	def show(self):
+		print("Name == " + self.name)
+		print("Address == " + self.address)
+		print("Emai == " + self.email)
+		print("Phone Number == " + self.phone)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
